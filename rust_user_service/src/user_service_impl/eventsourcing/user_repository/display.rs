183a2682546298f97122f34c9e59dda4a5d00a2d
amita <amita.yadav@knoldus.in>
2019-01-15 11:45:30 +0530

    added unit test case in display.rs

--- rust_user_service/src/user_service_impl/eventsourcing/user_repository/display.rs
+++ rust_user_service/src/user_service_impl/eventsourcing/user_repository/display.rs
@@ -1,9 +1,7 @@
-//use core::borrow::BorrowMut;
 use std::cell::RefCell;
 
 use cdrs::{self, types::prelude::*};
 use cdrs::query::QueryExecutor;
-//use tramp::{Rec, tramp};
 
 use crate::user_service_impl::constants::queries::SELECT_QUERY;
 use crate::user_service_impl::env_setup::connection::CurrentSession;
@@ -19,7 +17,7 @@ pub fn select_user(session: &CurrentSession, user_id: String) -> Vec<UserMapper>
         .expect("get body")
         .into_rows()
         .expect("into rows");
-    //convert(user_state_rows)
+
     let get_user_list: RefCell<Vec<UserMapper>> = RefCell::new(vec![]);
     for row in user_state_rows {
         get_user_list.borrow_mut().push(UserMapper::try_from_row(row).expect("into get user"));
@@ -27,24 +25,6 @@ pub fn select_user(session: &CurrentSession, user_id: String) -> Vec<UserMapper>
     let result: Vec<UserMapper> =get_user_list.borrow().to_vec();
     result
 }
-
-/*///tail recursive call for retrieving user from user_state
-fn convert(rows: Vec<Row>) -> Vec<GetUser> {
-    let index: i32 = 0;
-    let acc: RefCell<Vec<GetUser>> = RefCell::new(vec![]);
-    fn sub_convert(rows: Vec<Row>, acc: RefCell<Vec<GetUser>>, index: i32) -> Rec<Vec<GetUser>>
-    {
-        let r_size: usize = rows.len();
-        if r_size != 0 {
-            let get_user: GetUser = GetUser::try_from_row(rows[index]).expect("into get user");
-            acc.borrow_mut().push(get_user);
-            rec_call!(sub_convert(rows, acc,index+1))
-        } else {
-            rec_ret!(acc.borrow().to_vec())
-        }
-    }
-    tramp(sub_convert(rows, acc, index))
-}*/
 
 /// select_all_user is used to retrieve list of all users' details
 pub fn select_all_user(session: &CurrentSession) -> Vec<UserMapper> {
@@ -62,4 +42,13 @@ pub fn select_all_user(session: &CurrentSession) -> Vec<UserMapper> {
     }
     let result: Vec<UserMapper> =get_user_list.borrow().to_vec();
     result
+}
+
+#[test]
+fn test_select_user(){
+   use crate::user_service_impl::env_setup::connection::connect;
+   assert_eq!(select_user(&connect(),
+                          "3275d519-28e5-5707-94a6-d16fac19835f".to_string()),
+"{'id':'3275d519-28e5-5707-94a6-d16fac19835f','user_state':{'user':{'id':'3275d519-28e5-5707-94a6-d16fac19835f',
+'name':'rohit','email':'rahul@gmail.com','password':'qwt'},'generation':'1'}}")
 }

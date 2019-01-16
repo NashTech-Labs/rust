d584f843f61024c85592c04191e21021820c18f2
amita <amita.yadav@knoldus.in>
2019-01-16 13:51:26 +0530

    Resolve conflict on rust user service

--- rust_user_service/src/user_service_impl/controller/handler.rs
+++ rust_user_service/src/user_service_impl/controller/handler.rs
@@ -2,8 +2,6 @@ use std::cell::RefCell;
 
 use actix_web::*;
 use eventsourcing::Aggregate;
-use bytes::Bytes;
-use futures::stream::once;
 use uuid::Uuid;
 
 use crate::user_service_impl::constants::constant::TAKE_FIRST;
@@ -75,30 +73,6 @@ pub fn get_user(data: State<AppState>, user_id: Path<String>)
         let user_state: UserState = serde_json::
         from_str(&result[TAKE_FIRST].user_state).unwrap();
         Ok(Json(map_user(user_state.user)))
-<<<<<<< HEAD
-    }
-}
-
-pub fn get_all_users(req: &HttpRequest<AppState>) -> HttpResponse {
-    let users: Vec<UserMapper> = select_all_user(&req.state().session);
-    let user_list: RefCell<Vec<User>> = RefCell::new(vec![]);
-    if users.is_empty() {
-        //Err(CustomError::InternalError { field: "error in getting all users" })
-        HttpResponse::new(http::StatusCode::INTERNAL_SERVER_ERROR)
-    } else {
-        for user in users {
-            let user_state: UserState = serde_json::from_str(&user.user_state).unwrap();
-            user_list.borrow_mut().push(map_user(user_state.user));
-        }
-
-       let array = user_list.borrow().to_vec();
-        let bytes = bincode::serde::serialize(&array,bincode::SizeLimit::Infinite).unwrap();
-        let user_byte_array = bincode::serde::deserialize(&bytes).unwrap();
-
-        HttpResponse::Ok()
-            .chunked().
-            body(Body::Streaming(Box::new(once(Ok(Bytes::from_static(user_byte_array))))))
-=======
     }
 }
 
@@ -133,7 +107,6 @@ pub fn get_all_users(req: &HttpRequest<AppState>) -> impl Responder {
         let vec_of_user: Vec<User> = user_list.borrow().to_vec();
 
         Ok(wrap_vec(vec_of_user))
->>>>>>> 94334322fddb5eacaafb99cc2707c5f28874c647
     }
 }
 
@@ -142,15 +115,12 @@ pub fn user_login(data: State<AppState>, user_login: Json<UserLogin>)
                   -> Result<String, CustomError> {
     let u_login: UserLogin = user_login.into_inner();
     let user_email: String = u_login.email;
-<<<<<<< HEAD
     let user_id: Uuid = get_id_by_email(&user_email);
     let user_status: Vec<UserMapper> = select_user(&data.session,
                                                    user_id.clone().to_string());
-=======
     let user_id: String= get_id_by_email(&user_email).to_string();
     let user_status: Vec<UserMapper> = select_user(&data.session,
                                                    user_id.clone());
->>>>>>> 94334322fddb5eacaafb99cc2707c5f28874c647
     if user_status.is_empty() {
         Err(CustomError::InvalidInput { field: "user not found" })
     } else {

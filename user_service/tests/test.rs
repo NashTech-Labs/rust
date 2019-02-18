3ab9ae5af7750e8e61bf4941abcbd861504ddf5d
amita <amita.yadav@knoldus.in>
2019-02-18 12:42:39 +0530

    Improve test cases, not system dependent

--- user_service/tests/test.rs
+++ user_service/tests/test.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate serde_json;
+extern crate cdrs;
 
 use actix_web::client::ClientRequest;
 use actix_web::test::TestServer;
@@ -13,9 +14,12 @@ use user::user_service_api::user_service::AppState;
 use user::db_connection::connect;
 use user::user_service_impl::handler::UserInfo;
 use user::user_service_api::user_service::UserService;
+use cdrs::query::QueryExecutor;
+use user::user_service_impl::env_setup::initializer;
 
 #[cfg_attr(tarpaulin, skip)]
 fn create_app() -> App<AppState> {
+    initializer(&connect());
     App::with_state(AppState { session: connect() })
         .resource("/create_user", |r| {
             r.method(http::Method::POST).with_async(UserInfo::create_user)
@@ -52,31 +56,56 @@ fn test_insert_first_time() {
         json!({"email": "sid@gmail.com", "id":
      "a9c8536e-75ee-582b-a145-b6ace45abe9d", "name" : "sid"})
     );
+
+    connect().query("DELETE from user_event_sourcing_ks.user_states WHERE user_id = 'a9c8536e-75ee-582b-a145-b6ace45abe9d'")
+        .expect("Deletion error in insert handler test");
 }
 
 #[test]
 fn test_insert_not_first_time() {
     let user_reg: UserRegistration = UserRegistration {
-        name: "rahul".to_string(),
-        email: "rsb007@gmail.com".to_string(),
-        password: "rsb007@".to_string(),
+        name: "sid".to_string(),
+        email: "sid@gmail.com".to_string(),
+        password: "sid123@".to_string(),
     };
     let mut server: TestServer = test::TestServer::with_factory(create_app);
+
+    {
+        let request: ClientRequest = server
+            .client(http::Method::POST, "/create_user")
+            .json(user_reg.clone())
+            .unwrap();
+        let _response: ClientResponse = server.execute(request.send()).unwrap();
+    }
     let request: ClientRequest = server
         .client(http::Method::POST, "/create_user")
         .json(user_reg)
         .unwrap();
+
     let response: ClientResponse = server.execute(request.send()).unwrap();
+
     assert!(response.status().is_client_error());
+    connect().query("DELETE from user_event_sourcing_ks.user_states WHERE user_id = 'a9c8536e-75ee-582b-a145-b6ace45abe9d'")
+        .expect("Deletion error in insert handler test");
 }
 
 #[test]
 fn test_display_by_id() {
     let mut server: TestServer = test::TestServer::with_factory(create_app);
+    let user_reg: UserRegistration = UserRegistration {
+        name: "sid".to_string(),
+        email: "sid@gmail.com".to_string(),
+        password: "sid123@".to_string(),
+    };
+    let request: ClientRequest = server
+        .client(http::Method::POST, "/create_user")
+        .json(user_reg)
+        .unwrap();
+    let _response: ClientResponse = server.execute(request.send()).unwrap();
     let request: ClientRequest = server
         .client(
             http::Method::GET,
-            "/get_user/3275d519-28e5-5707-94a6-d16fac19835f",
+            "/get_user/a9c8536e-75ee-582b-a145-b6ace45abe9d",
         )
         .finish()
         .unwrap();
@@ -87,19 +116,30 @@ fn test_display_by_id() {
     let user_detail_json: Value = serde_json::from_str(parsed_user_detail).unwrap();
     assert_eq!(
         user_detail_json,
-        json!({"id": "3275d519-28e5-5707-94a6-d16fac19835f","name": "rohit","email":
-    "rahul@gmail.com"})
+        json!({"id": "a9c8536e-75ee-582b-a145-b6ace45abe9d","name": "sid","email":
+    "sid@gmail.com"})
     );
+    connect().query("DELETE from user_event_sourcing_ks.user_states WHERE user_id = 'a9c8536e-75ee-582b-a145-b6ace45abe9d'")
+        .expect("Deletion error in insert handler test");
 }
 
 #[test]
 fn test_user_login() {
-    let user_login: UserLogin = UserLogin {
-        email: "rsb007@gmail.com".to_string(),
-        password: "rsb007@".to_string(),
+    let user_reg: UserRegistration = UserRegistration {
+        name: "sid".to_string(),
+        email: "sid@gmail.com".to_string(),
+        password: "sid123@".to_string(),
     };
     let mut server: TestServer = test::TestServer::with_factory(create_app);
-
+    let request: ClientRequest = server
+        .client(http::Method::POST, "/create_user")
+        .json(user_reg)
+        .unwrap();
+    let _response: ClientResponse = server.execute(request.send()).unwrap();
+    let user_login: UserLogin = UserLogin {
+        email: "sid@gmail.com".to_string(),
+        password: "sid123@".to_string(),
+    };
     let request: ClientRequest = server
         .client(http::Method::POST, "/login")
         .json(user_login)
@@ -108,7 +148,7 @@ fn test_user_login() {
 
     let user_id = server.execute(response.body()).unwrap();
     let parsed_user_id = str::from_utf8(&user_id).unwrap();
-    assert_eq!(parsed_user_id, "f95dfd0b-e2fa-5b88-a284-578f9a015f4d");
+    assert_eq!(parsed_user_id, "a9c8536e-75ee-582b-a145-b6ace45abe9d");
 }
 
 #[test]
